3cca74a35dd27a7c4ebf351dc116fb54d600b9ed
akashca-pro <akashca656@gmail.com>
2025-07-22 21:23:48 +0530

    feat (user) : added dto for representing user to public

---
+++ src/domain/dtos/User/PublicUser.ts
@@ -0,0 +1,16 @@
+/**
+ * Data Transfer Object (DTO) representing user data for public-facing views like leaderboard.
+ *
+ * @interface
+ */
+export interface PublicUserDTO {
+  username: string
+  avatar?: string
+  country: string
+  preferredLanguage?: string
+  easySolved: number
+  mediumSolved: number
+  hardSolved: number
+  totalSubmission: number
+  streak: number
+}
